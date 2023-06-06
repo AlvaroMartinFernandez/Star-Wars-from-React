ae511c8bc053b0e7d835d914ab1de2a6ddda5e52
Lucas Solari <solarilucas92@gmail.com>
2023-06-06 14:45:34 +0000

    logo star wars

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,19 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import img from "../../img/star-wars.png";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<div class="dropdown">
-  <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-    Dropdown button
-  </button>
-</div>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="navbar navbar-light bg-light mb-3">
+      <Link to="/">
+        <span className="navbar-brand mb-0 h1">
+          <img className="ms-5" src={img} />
+        </span>
+      </Link>
+      <div className="ml-auto">
+        <div class="dropdown">
+          <button
+            className="btn btn-secondary dropdown-toggle me-5"
+            type="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            Dropdown button
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
 };
